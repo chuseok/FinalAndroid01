d645e7c10da3a1979cce0bfe1d6f02e53286df42
chuseok <90127lucy@gmail.com>
2020-09-04 19:01:58 +0900

    display 적용

--- LoginTest/app/src/main/java/com/example/logintest/RegisterActivity.java
+++ LoginTest/app/src/main/java/com/example/logintest/RegisterActivity.java
@@ -5,13 +5,25 @@ import androidx.appcompat.widget.Toolbar;
 
 import android.os.Bundle;
 import android.view.MenuItem;
+import android.widget.TextView;
+
+import com.example.logintest.Utils.MobileSize;
 
 public class RegisterActivity extends AppCompatActivity {
+
+    TextView authTextView;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_register);
+
+        MobileSize mobileSize = new MobileSize();
+        mobileSize.getStandardSize(this);
+
+        authTextView = findViewById(R.id.authTextView);
+        authTextView.setTextSize((float) (mobileSize.getStandardSize_X()/3));
+
 
         Toolbar mToolbar = (Toolbar) findViewById(R.id.main_toolbar);
         setSupportActionBar(mToolbar);
