e80c8a5d0aed8c795072a76cec9fe2ac79c89404
chuseok <90127lucy@gmail.com>
2020-09-04 13:30:17 +0900

    loginActivity에 signup textView 추가

--- LoginTest/app/src/main/java/com/example/logintest/RegisterActivity.java
+++ LoginTest/app/src/main/java/com/example/logintest/RegisterActivity.java
@@ -1,8 +1,10 @@
 package com.example.logintest;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.appcompat.widget.Toolbar;
 
 import android.os.Bundle;
+import android.view.MenuItem;
 
 public class RegisterActivity extends AppCompatActivity {
 
@@ -10,5 +12,21 @@ public class RegisterActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_register);
+
+        Toolbar mToolbar = (Toolbar) findViewById(R.id.main_toolbar);
+        setSupportActionBar(mToolbar);
+
+        getSupportActionBar().setDisplayHomeAsUpEnabled(true);
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        switch (item.getItemId()){
+            case android.R.id.home:{ //toolbar의 back키 눌렀을 때 동작
+                finish();
+                return true;
+            }
+        }
+        return super.onOptionsItemSelected(item);
     }
 }
